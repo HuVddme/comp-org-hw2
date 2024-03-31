4ac7726871d0e90690b5b02540a0353831b4f87d
HuVddme <valentine.ezikeoha@bison.howard.edu>
2024-03-30 23:33:04 -0400

    creating arrays

--- memory_benchmark/memory_benchmark.cpp
+++ memory_benchmark/memory_benchmark.cpp
@@ -2,3 +2,11 @@
 #include <chrono>
 using namespace std;
 using namespace std::chrono;
+
+int main() {
+    
+    const long long size_array = 5000000000;
+  
+    int* array_data = new int[size_array]; 
+    int temp_data;
+}
