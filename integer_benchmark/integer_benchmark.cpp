5e815bd71ee75bd6d935dc0ee61ba23be32ea2a9
HuVddme <valentine.ezikeoha@bison.howard.edu>
2024-03-30 22:35:08 -0400

    addition loop

--- integer_benchmark/integer_benchmark.cpp
+++ integer_benchmark/integer_benchmark.cpp
@@ -8,7 +8,16 @@ int main()
 
     const int exp_billion = 1e9;
 
-    int32_t a = 2;
-    int32_t b = 2;
+    int32_t k = 2;
+    int32_t l = 2;
     int32_t result;
+
+    auto start_main = high_resolution_clock::now();
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < exp_billion; j++)
+        {
+            result = k + l;
+        }
+    }
 }
