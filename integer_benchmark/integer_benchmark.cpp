86a1811e86daa9f756149062efd8ee7ac486bd1a
HuVddme <valentine.ezikeoha@bison.howard.edu>
2024-03-30 22:26:48 -0400

    initialize main funct and var

--- integer_benchmark/integer_benchmark.cpp
+++ integer_benchmark/integer_benchmark.cpp
@@ -2,3 +2,13 @@
 #include <chrono>
 using namespace std;
 using namespace std::chrono;
+
+int main()
+{
+
+    const int exp_billion = 1e9;
+
+    int32_t a = 2;
+    int32_t b = 2;
+    int32_t result;
+}
