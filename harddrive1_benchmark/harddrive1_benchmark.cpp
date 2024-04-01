35ebf621898ecc529aa65a23efa6a7381593ef11
HuVddme <valentine.ezikeoha@bison.howard.edu>
2024-04-01 16:34:07 -0400

    char array to store sample data

--- harddrive1_benchmark/harddrive1_benchmark.cpp
+++ harddrive1_benchmark/harddrive1_benchmark.cpp
@@ -5,9 +5,15 @@ using namespace std;
 using namespace std::chrono;
 
 const long long total_bytes = 1e9;
-const int section_size = 100;
+const int sect_size = 100;
 
 int main() {
     ofstream outFile("sample.txt", ios::binary);
+
+    char buffer[sect_size];
+    for (int i = 0; i < sect_size; i++) {
+        buffer[i] = 'x';
+    
+    }
 
 }
