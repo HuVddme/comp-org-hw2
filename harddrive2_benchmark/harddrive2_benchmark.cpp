1949d1df0282262ab918a66d3835f8f6691ccd20
HuVddme <valentine.ezikeoha@bison.howard.edu>
2024-04-01 17:47:54 -0400

    var created, open main

--- harddrive2_benchmark/harddrive2_benchmark.cpp
+++ harddrive2_benchmark/harddrive2_benchmark.cpp
@@ -4,3 +4,10 @@
 
 using namespace std;
 using namespace std::chrono;
+
+const long long TOTAL_BYTES = 1e9;
+const int SECTION_SIZE = 10000;
+
+int main() {
+    ofstream outputFile("sample.txt", ios::binary);
+}
