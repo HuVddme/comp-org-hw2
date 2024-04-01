36ac0f29951f0a54388bca1dfde481d841689a80
HuVddme <valentine.ezikeoha@bison.howard.edu>
2024-04-01 16:31:07 -0400

    output file stream for binary data

--- harddrive1_benchmark/harddrive1_benchmark.cpp
+++ harddrive1_benchmark/harddrive1_benchmark.cpp
@@ -3,3 +3,11 @@
 #include <chrono>
 using namespace std;
 using namespace std::chrono;
+
+const long long total_bytes = 1e9;
+const int section_size = 100;
+
+int main() {
+    ofstream outFile("sample.txt", ios::binary);
+
+}
